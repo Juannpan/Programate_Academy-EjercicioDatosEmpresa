9f6da3598d1ae899377f32ffde6660b419543101
juannpan <camiloserranope@gmail.com>
2024-02-20 21:02:12 -0500

    creada tabla de frecuencia de planta utilizando la base de datos

---
+++ Database/datosplanta.py
@@ -0,0 +1,20 @@
+cp_plant = [
+    {'planta': 'TelaTech Factory', 'precio': 4.55},
+    {'planta': 'FiberWorks Manufacturing', 'precio': 4.34},
+    {'planta': 'SilkStream Textiles', 'precio': 4.27},
+    {'planta': 'CottonCraft Works', 'precio': 4.13},
+    {'planta': 'LinenLoom Productions', 'precio': 4.9},
+    {'planta': 'ThreadTide Textile Mill', 'precio': 4.34},
+    {'planta': 'WeaveWave Fabrics', 'precio': 4.72},
+    {'planta': 'YarnYield Manufacturing', 'precio': 4.3},
+    {'planta': 'DyeDreams Factory', 'precio': 4.51},
+    {'planta': 'KnitNest Textile Co.', 'precio': 4.67}
+]
+
+lugares = [
+    {"lugar": "EcoChic Boutique", "descuento": 30, "envio": 3},
+    {"lugar": "Vintage Vogue Emporium", "descuento": 35, "envio": 4},
+    {"lugar": "FitFrenzy Activewear", "descuento": 33, "envio": 3},
+    {"lugar": "Little Sprouts Fashion House", "descuento": 30, "envio": 3},
+    {"lugar": "Local Threads Collective", "descuento": 40, "envio": 5}
+]
